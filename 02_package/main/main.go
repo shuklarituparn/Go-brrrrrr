5cb8c66e505bf39ffb0d88348b7fb9b0ff189edb
Rituparn Shukla <rituparn_shukla@outlook.com>
2023-04-23 19:59:43 +0300

    added info on go packages

--- 02_package/main/main.go
+++ 02_package/main/main.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/GoesToEleven/GolangTraining/02_package/stringutil"
-	"github.com/GoesToEleven/GolangTraining/02_package/icomefromalaska"
-	//someAlias "github.com/GoesToEleven/GolangTraining/02_package/icomefromalaska"
+	"github.com/shuklarituparn/Go-brrrrrr/02_package/icomefromalaska"
+	"github.com/shuklarituparn/Go-brrrrrr/02_package/stringutil"
 )
 
 func main() {
@@ -12,3 +11,13 @@ func main() {
 	fmt.Println(stringutil.MyName)
 	fmt.Println(winniepooh.BearName)
 }
+
+/*In order to implement a file or a package in GO and to use
+it, go to the folder, if you're hosting that file on the github
+then initialize the go mod there
+
+go mod init github.com/shuklarituparn/Go-brrrrrr/02_package
+
+it will create a go.mod file in the repo containing the code that we wanna use and then we can use
+using just import keyword.
+*/
