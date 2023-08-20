3bb54be48d9e8f810484921f72bba15c9ac62e64
shukla rituparn <shukla.r@phystech.edu>
2023-08-20 11:09:05 +0300

    added slices

--- 18_slice/12_multi-dimensional/04_comparing_shorthand_var_make/01_shorthand-slice/main.go
+++ 18_slice/12_multi-dimensional/04_comparing_shorthand_var_make/01_shorthand-slice/main.go
@@ -5,10 +5,10 @@ import (
 )
 
 func main() {
-	student := []string{}
+	student := []string{} 
 	students := [][]string{}
-	student[0] = "Todd"
-	// student = append(student, "Todd")
-	fmt.Println(student)
-	fmt.Println(students)
+	//student[0] = "Todd"  //thsis doesn't work and gives out of bounds error
+	student = append(student, "Todd")   //this works
+	fmt.Println(student)     
+	fmt.Println(students)  //students is an empty slice containing a slice
 }
